8cdd7e6240320feb1aba982746db78ea3f1bd8ad
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-01-17 08:48:02 +0000

    Jplayer now working on Firefox and Opera - checking in for the night

--- branches/rh-jplayer-0.2.1/apps/client/modules/player_desktop/templates/_load_html5_player.php
+++ branches/rh-jplayer-0.2.1/apps/client/modules/player_desktop/templates/_load_html5_player.php
@@ -27,35 +27,41 @@ else if( strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Safari' ) && strpos( $_SERVER[ 
   echo '<audio preload="none" controls="" id="musicplayer" class="safari-ipad"></audio>' . "\r\n";
 }
 
-//firefox lacks support for HTML5 MP3 codec playback, so we'll use jPlayer and get support from flash (sigh)
-else if( strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Firefox' ))
+//firefox and opera lack support for HTML5 MP3 codec playback,
+//so we'll use jPlayer and get support from flash (sigh)
+else if( 
+         strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Firefox' )
+         ||
+         strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'pera/' )
+        )
 {
   use_stylesheet( '/css/jPlayer.Skin/jplayer.blue.monday.css');
   use_javascript( '/js/jQuery.jPlayer.2.0.0/jquery.jplayer.min.js' );
   echo <<<EOL
 <div id="jquery_jplayer_1" class="jp-jplayer"></div>
-  <div class="jp-audio">
-    <div class="jp-type-single">
-      <div id="jp_interface_1" class="jp-interface" style="top: -10px; left: -20px; border: none; background: none">
-        <ul class="jp-controls">
-          <li><a href="#" class="jp-play" tabindex="1">play</a></li>
-          <li><a href="#" class="jp-pause" tabindex="1">pause</a></li>
-          <li><a href="#" class="jp-stop" tabindex="1">stop</a></li>
-          <li><a href="#" class="jp-mute" tabindex="1">mute</a></li>
-          <li><a href="#" class="jp-unmute" tabindex="1">unmute</a></li>
-        </ul>
+<div class="jp-audio">
+  <div class="jp-type-single">
+    <div id="jp_interface_1" class="jp-interface">
+      <ul class="jp-controls">
+        <li><a href="#" class="jp-play" tabindex="1">play</a></li>
+        <li><a href="#" class="jp-pause" tabindex="1">pause</a></li>
+        <li><a href="#" class="jp-stop" tabindex="1">stop</a></li>
+        <li><a href="#" class="jp-mute" tabindex="1">mute</a></li>
+        <li><a href="#" class="jp-unmute" tabindex="1">unmute</a></li>
+      </ul>
+      <div class="jp-volume-bar">
+        <div class="jp-volume-bar-value"></div>
+      </div>
+      <div class="jp-seekcontainer">
         <div class="jp-progress">
           <div class="jp-seek-bar">
             <div class="jp-play-bar"></div>
           </div>
         </div>
-        <div class="jp-volume-bar">
-          <div class="jp-volume-bar-value"></div>
-        </div>
         <div class="jp-current-time"></div>
         <div class="jp-duration"></div>
       </div>
-    <div>
+    </div>
   </div>
 </div>
 EOL;
