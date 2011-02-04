158b381a46f0c72c04e9b9fa9575583446cd673a
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-02-04 04:52:19 +0000

    adding language chooser system. deleting untranslated xliff files for now

--- branches/rh-languages-0.3.0/apps/client/lib/i18nAutodetectFilter.class.php
+++ branches/rh-languages-0.3.0/apps/client/lib/i18nAutodetectFilter.class.php
@@ -1,7 +1,10 @@
 <?php
+#
+# Culture management system for Streeme.
+# Cheers to Symfonians for i18nSubdomainFilter which showed me what to do :) I owe someone a beer 
+#
 class i18nAutodetectFilter extends sfFilter
 {
-
   /**
    * Log filter activity
    *
@@ -10,7 +13,7 @@ class i18nAutodetectFilter extends sfFilter
    */
   public function log($message, $level = sfLogger::DEBUG)
   {
-    sfContext::getInstance()->getLogger()->log('{i18nSubdomainFilter} '.$message, $level);
+    sfContext::getInstance()->getLogger()->log('{i18nAutodetectFilter} '.$message, $level);
   }
 
   /**
@@ -24,23 +27,38 @@ class i18nAutodetectFilter extends sfFilter
     {
       $context = $this->getContext();
       $request = $context->getRequest();
-      $user    = $context->getUser();
+      $response = $context->getResponse();
+      $user = $context->getUser();
+      $cookieName = sfConfig::get( 'app_translation_cookie', 'myCulture' );
       
-      //get the user's preferred (browser) language
-      $lang    =  $request->getPreferredCulture( sfConfig::get('sf_translations_available', array() ) );
+      //get the user's preferred browser language
+      $culture =  $request->getPreferredCulture( sfConfig::get('sf_translations_available', array() ) );
+      $this->log(sprintf( 'Browser language is %s', $culture ) );
       
-      //has the user overridden the language?
-      if( $request->getParameter('sf_culture') && in_array( $request->getParameter('sf_culture'),  sfConfig::get('sf_translations_available', array() ) ) )
+      //override with a user cookie language if it exists
+      if( $request->getCookie($cookieName) )
       {
-        $lang = $request->getParameter('sf_culture');
+        $culture = $request->getCookie($cookieName);
+        $this->log(sprintf( 'Overriding culture %s from culture cookie', $culture ) );
       }
       
-      $this->log(sprintf('Applying detected requested lang to session: %s', $lang));
-      $user->setCulture( $lang );
-      sfConfig::set('sf_current_culture', $lang );
-      $context->getResponse()->addMeta( 'language', $lang, true );
+      //has the user overridden the language manually?
+      if( $request->getParameter('sf_culture') && in_array( $request->getParameter('sf_culture'),  sfConfig::get('sf_translations_available', array() ) ) )
+      {
+        //set the language for this request, so you don't have to refresh
+        $culture = $request->getParameter('sf_culture');
+        
+        //update the cookie with the new language for future requests
+        $response->setCookie( $cookieName, $culture, time()+60*60*24*30*12*30, '/' );
+        
+        $this->log(sprintf( 'Culture manually overridden to %s', $culture ) );
+      }
+      
+      $this->log(sprintf('Applying culture: %s for this session', $culture));
+      $user->setCulture( $culture );
+      sfConfig::set('sf_current_culture', $culture );
+      $context->getResponse()->addMeta( 'language', $culture, true );
     }
     $filterChain->execute();
   }
-
 }
