2bfa6e4ac8c9f854965b62d42d589b15ecb33aec
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-04-28 14:54:55 +0000

    checking in - adding more functional tests - fixing discovered bugs in service actions

--- branches/rh-playlists-0.4.1/test/functional/client/serviceActionsTest.php
+++ branches/rh-playlists-0.4.1/test/functional/client/serviceActionsTest.php
@@ -28,7 +28,8 @@ $browser->
   
   info('3. Read the new playlist')->
   
-  get('/service/listPlaylists', array( 'alpha' => 't' ))->
+  info('Without an alpha letter')->
+  get('/service/listPlaylists', array())->
   
   with('request')->begin()->
     isParameter('module', 'service')->
@@ -37,6 +38,65 @@ $browser->
   
   with('response')->begin()->
     isStatusCode(200)->
-    isHeader('Content-Type', 'application/json')->
-  end()
+    matches('/\[\{\"id\"\:\"1\",\"name\"\:\"Default Playlist\"\},\{\"id\"\:\"2\",\"name\"\:\"testplaylist\"\}\]/')->
+  end()->
+  
+  info('With alpha letter "t"')->
+  get('/service/listPlaylists', array( 'alpha' => 't' ))->
+  
+  with('response')->begin()->
+    isStatusCode(200)->
+    matches('/\[\{\"id\"\:\"2\",\"name\"\:\"testplaylist\"\}\]/')->
+  end()->
+  
+  info('4. Add content to the playlist')->
+
+  info('no playlist specified')->
+  post('/service/addPlaylistContent', array())->
+  
+  with('request')->begin()->
+    isParameter('module', 'service')->
+    isParameter('action', 'addPlaylistContent')->
+  end()->
+  
+  with('response')->begin()->
+    isStatusCode(404)->
+  end()->
+  
+  info('Playlist 2 specified adding song 1')->
+  post('/service/addPlaylistContent', array('playlist_id'=>'2', 'id' =>'9qw9dwj9wqdjw9qjqw9jd', 'type' => 'song'))->
+  with('response')->begin()->
+    isStatusCode(200)->
+  end()->
+
+  post('/service/deletePlaylistContent', array())->
+  
+  with('request')->begin()->
+    isParameter('module', 'service')->
+    isParameter('action', 'deletePlaylistContent')->
+  end()->
+  
+  with('response')->begin()->
+    isStatusCode(404)->
+  end()->
+  
+  info('Playlist 2 specified deleting song 1')->
+  post('/service/addPlaylistContent', array('playlist_id'=>'2', 'id' =>'9qw9dwj9wqdjw9qjqw9jd', 'type' => 'song'))->
+  with('response')->begin()->
+    isStatusCode(200)->
+  end()->
+  
+  info('5. Delete the test playlist')->
+  
+  post('/service/deletePlaylist', array('playlist_id' => '2'))->
+  with('response')->begin()->
+    isStatusCode(200)->
+  end()->
+  
+  get('/service/listPlaylists', array( 'alpha' => 't' ))->
+  
+  with('response')->begin()->
+    isStatusCode(200)->
+    matches('/\[\]/')->
+  end()  
 ;
