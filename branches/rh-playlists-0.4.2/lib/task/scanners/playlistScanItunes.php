a24afd4dd4ab3a32e452dbab579c8348884fa8f4
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-06-19 05:15:10 +0000

    adding beginning of playlist scanner and echonest integrations

---
+++ branches/rh-playlists-0.4.2/lib/task/scanners/playlistScanItunes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * playlistScanItunes
+ *
+ * Itunes playlist ingest process
+ *
+ * @package    streeme
+ * @author     Richard Hoar
+ * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
+ */
+$itunes_music_library   = sfConfig::get( 'app_itunes_xml_location' );
+$mapped_drive_locations = sfConfig::get( 'app_mdl_mapped_drive_locations' );
+$allowed_filetypes      = array_map( 'strtolower', sfConfig::get( 'app_aft_allowed_file_types' ) );
+$playlist_scanner       = new PlaylistScan();
+$itunes_parser          = new StreemeItunesTrackParser( $itunes_music_library );
+
+while( $value = $itunes_parser->getTrack() )
+{
+}
+?>
