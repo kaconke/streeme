6953db703d490432444272421d7bc4f1891a3845
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-07-17 08:07:52 +0000

    adding playlist scanner task code

--- lib/task/scanners/playlistScanItunes.php
+++ lib/task/scanners/playlistScanItunes.php
@@ -12,10 +12,24 @@ $itunes_music_library   = sfConfig::get( 'app_itunes_xml_location' );
 $mapped_drive_locations = sfConfig::get( 'app_mdl_mapped_drive_locations' );
 $allowed_filetypes      = array_map( 'strtolower', sfConfig::get( 'app_aft_allowed_file_types' ) );
 $itunes_parser          = new StreemeItunesPlaylistParser( $itunes_music_library );
-
+$playlist_scan          = new PlaylistScan('itunes');
 $playlist_name = $itunes_playlist_id = null;
 $playlist_songs = array();
 while( $itunes_parser->getPlaylist( $playlist_name, $itunes_playlist_id, $playlist_songs ) )
 {
-  echo $playlist_name;
+  $playlist_id = $playlist_scan->is_scanned( 
+                                             $playlist_scan->get_service_name(),
+                                             $playlist_name,
+                                             $itunes_playlist_id
+                                           );
+  $playlist_id = $playlist_scan->add_playlist( 
+                                             $playlist_name,
+                                             $playlist_songs,
+                                             $playlist_id,
+                                             $playlist_scan->get_last_scan_id(),
+                                             $playlist_scan->get_service_name(),
+                                             $itunes_playlist_id
+                                           );
 }
+$playlist_scan->finalize_scan(PlaylistFilesTable::getInstance());
+echo $playlist_scan->get_summary();
