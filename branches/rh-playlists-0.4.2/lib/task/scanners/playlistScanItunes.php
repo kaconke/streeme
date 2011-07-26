1bcce4fdb3fdb0fe8dea0f41e76e25c96eeb4207
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-07-26 05:25:26 +0000

    Correcting playlist scanner and list behaviour to avoid placing empty itunes playlists in the results

--- branches/rh-playlists-0.4.2/lib/task/scanners/playlistScanItunes.php
+++ branches/rh-playlists-0.4.2/lib/task/scanners/playlistScanItunes.php
@@ -15,14 +15,32 @@ $itunes_parser          = new StreemeItunesPlaylistParser( $itunes_music_library
 $playlist_scan          = new PlaylistScan('itunes');
 $playlist_name = $itunes_playlist_id = null;
 $playlist_songs = array();
+function mapItunes($collection)
+{
+  return array('filename'=>StreemeUtil::itunes_format_decode($collection['filename']));
+}
 while( $itunes_parser->getPlaylist( $playlist_name, $itunes_playlist_id, $playlist_songs ) )
 {
-  $playlist_id = $playlist_scan->is_scanned( 
+  //There's no point scanning the entire library again, so we'll exclude the first record in iTunes
+  if(!isset($first_skipped))
+  {
+    $first_skipped = true;
+    continue;
+  }
+  
+  //convert itunes filenames to system specific paths
+  $playlist_songs = array_map(mapItunes, $playlist_songs);
+  
+  //update playlists
+  if(count($playlist_songs) > 0)
+  {
+    $playlist_id = $playlist_scan->is_scanned(
                                              $playlist_scan->get_service_name(),
                                              $playlist_name,
                                              $itunes_playlist_id
                                            );
-  $playlist_id = $playlist_scan->add_playlist( 
+
+    $playlist_id = $playlist_scan->add_playlist(
                                              $playlist_name,
                                              $playlist_songs,
                                              $playlist_id,
@@ -30,6 +48,7 @@ while( $itunes_parser->getPlaylist( $playlist_name, $itunes_playlist_id, $playli
                                              $playlist_scan->get_service_name(),
                                              $itunes_playlist_id
                                            );
+  }
 }
 $playlist_scan->finalize_scan(PlaylistFilesTable::getInstance());
 echo $playlist_scan->get_summary();
