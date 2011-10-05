27021761d6e5ed54c95e78ae94de3f8cf185861a
chaffneue <chaffneue@074d5385-786b-9dd0-d573-f941bf9f5f13>
2011-10-05 03:34:13 +0000

    updating echonest api class to provide more accessors for testing

--- apps/client/lib/StreemeEchonestConsumer.class.php
+++ apps/client/lib/StreemeEchonestConsumer.class.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * An echonest API consumer for Streeme
+ * An echonest API consumer for Streeme - this library requires sfWebBrowserPlugin for HTTP calls
  *
  * @author Richard Hoar
  * @package Streeme
- * @depends sfWebBrowser
+ * @depends sfWebBrowserPlugin
  */
 class StreemeEchonestConsumer
 {
-  public $format, $apiKey, $apiVersion, $http, $endPoint;
-  public $options = array();
+  protected $format, $apiKey, $apiVersion, $http, $apiEndpoint, $uri, $responseTime;
+  protected $parameters = array();
 
   /**
    * Constructor
@@ -17,60 +17,116 @@ class StreemeEchonestConsumer
    * @param apiKey str: Echonest Api Key
    * @param http   obj: sfWebBrowser instance
    */
-  public function __construct($apiKey, sfWebBrowser $http, $apiVersion = 'v4')
+  public function __construct($apiKey, sfWebBrowser $http, $apiEndpoint = 'http://developer.echonest.com/api', $apiVersion = 'v4')
   {
     $this->format = 'xml';
+    $this->http = $http;
     $this->apiKey = $apiKey;
     $this->apiVersion = $apiVersion;
-    $this->http = $http;
-    $this->endPoint = 'http://developer.echonest.com/api';
+    $this->apiEndpoint = $apiEndpoint;
   }
   
   /**
-   * Call echonest and get the results
+   * Call echonest and get the results for the given parameters set with setParameter
    *
    * @param api     str: the api to call (eg. artist, song, track)
    * @param service str: the api method to call (eg. search, profile)
+   * @param method  str: GET|POST
    * @return        str: xml result
    */
-  public function call($api, $service)
+  public function query($api, $service, $method = 'GET')
   {
-    //build the uri and send the request
-    if(is_array($this->options))
+    $time_start = microtime(true);
+    
+    $params = null;
+    if(is_array($this->parameters))
     {
-      foreach ($this->options as $option)
+      foreach ($this->parameters as $parameter)
       {
-        $params .= sprintf('&%s', http_build_query($option));
+        $params .= sprintf('&%s', http_build_query($parameter));
       }
     }
   
-    $uri = sprintf('%s/%s/%s/%s?api_key=%s&format=%s&%s', $this->endPoint, $this->apiVersion, $api, $service, $this->apiKey, $this->format, $params);
-    var_dump($uri);
-    $this->http->call($uri);
+    $this->uri = sprintf( '%s/%s/%s/%s?api_key=%s&format=%s%s', $this->apiEndpoint, $this->apiVersion, $api, $service, $this->apiKey, $this->format, $params );
+    
+    $this->http->call($this->uri, $method);
    
-    $this->options = array();
+    $this->parameters = array();
+    
+    $time_end = microtime(true);
+    $this->responseTime = $time_end - $time_start;
     
     return $this;
   }
   
+  /**
+   * Get the raw xml response for caching/string ops
+   * 
+   * @return         str: xml response from Echonest
+   */
   public function fetchRawResult()
   {
     return $this->http->getResponseText();
   }
-  
-  public function fetchResult()
-  {
-    return simplexml_load_string($this->http->getResponseText(),'SimpleXMLElement', LIBXML_NOCDATA);
-  }
 
   /**
-   * Set an option for the call
+   * Convert the XML response to a SimpleXML object 
    *
-   * @param name str: the key name
-   * @param value str: the corresponding value
+   * @return         obj: SimpleXMLElement
    */
-  public function setOption($name, $value)
+  public function fetchResult()
   {
-    $this->options[] = array($name=>$value);
+    return $this->http->getResponseXML();
+  }
+  
+  /**
+   * Convert the XML response to an associative array
+   * 
+   * @return         arr: results from enchonest as an array
+   */
+  public function fetchAssocResult()
+  {
+    return json_decode(json_encode($this->http->getResponseXML()),true);
+  }
+  
+  /**
+   * Set an url parameter for the call
+   *
+   * @param name     str: the key name
+   * @param value    str: the corresponding value
+   */
+  public function setParameter($name, $value)
+  {
+    $this->parameters[] = array($name=>$value);
+  }
+  
+  /**
+   * Get a list of parameters registered for the next call 
+   * 
+   * @return         arr: the parameters array
+   */
+  public function getParameters()
+  {
+    return $this->parameters;
+  }
+  
+  /**
+   * Get the URI of the most recent call
+   * 
+   * @return         str: the last requested url - call after ->query()
+   */
+  public function getUri()
+  {
+    return $this->uri;
+  }
+  
+  /**
+   * Get response time
+   * 
+   * @return         flt: time request took in ms 
+   */
+  public function getResponseTime()
+  {
+    return $this->responseTime;
   }
 }
